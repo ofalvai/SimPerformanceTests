af133c885e79eec6a2beb2346fa57a65c19e706b
Olivér Falvai <ofalvai@gmail.com>
2023-06-15 17:06:28 +0200

    Initial Commit

---
+++ SimulatorPerformance/SimulatorPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimulatorPerformanceApp.swift
+//  SimulatorPerformance
+//
+//  Created by Olivér Falvai on 2023. 06. 15..
+//
+
+import SwiftUI
+
+@main
+struct SimulatorPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
